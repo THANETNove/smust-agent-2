9aa73fa4bf296edeab243d989c89ffbda43773ff
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-11 09:25:11 +0700

    ปรับcss

--- resources/views/detall/reall_detall.blade.php
+++ resources/views/detall/reall_detall.blade.php
@@ -1,28 +1,28 @@
  {{-- เช่า  --}}
- <div>
-     <p class="text-content">{{ $home->details }}</p>
-     <p class="text-content-black margin-bottom-8">
-         <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/Vector.png') }}">
-         เช่าขั้นต่ำ <span class="ml-16">{{ number_format($home->minimum_rent) }}
-             เดือน</span>
-     </p>
-     <div class="space-between">
-         <p class="text-content-black margin-bottom-8">
-             <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-             เงินประกัน {{ number_format($home->deposit) }} บาท
+ <p class="text-content">{{ $home->details }}</p>
+ <p class="text-content-black margin-bottom-8">
+     <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/Vector.png') }}">
+     เช่าขั้นต่ำ <span class="ml-16">{{ number_format($home->minimum_rent) }}
+         เดือน</span>
+ </p>
+ <div class="flex-direction-break-word">
+     <div class="w-50">
+         <p class="text-content-black margin-bottom-8 space-between">
+             <span> <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                 เงินประกัน {{ number_format($home->deposit) }} บาท</span>
+             <span>-</span>
          </p>
-         <span>-</span>
+         <p class="text-content-black margin-bottom-8 space-between">
+             <span> <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                 ค่าเช่าล่วงหน้า {{ number_format($home->advance_rent) }} บาท</span>
+             <span>-</span>
+         </p>
+     </div>
+     <div class="w-50">
          <p class="text-content-black margin-bottom-8">
              <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
              เงินมัดจำ <span class="ml-16"> {{ $home->cash_pledge }} เดือน</span>
          </p>
-     </div>
-     <div class="space-between">
-         <p class="text-content-black margin-bottom-8">
-             <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-             ค่าเช่าล่วงหน้า {{ number_format($home->advance_rent) }} บาท
-         </p>
-         <span>-</span>
          <p class="text-content-black margin-bottom-8">
              <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
              เงินจอง <span class="ml-16">{{ number_format($home->advance_rent) }}
--- resources/views/detall/sell_detall.blade.php
+++ resources/views/detall/sell_detall.blade.php
@@ -2,34 +2,42 @@
   <div class="mt-3">
       <p class="text-content-black margin-bottom-8">
           <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/Vector.png') }}">
-          ราคาขาย <span class="ml-16">{{ number_format($home->advance_rent) }}
+          ราคาขาย <span class="ml-8">{{ number_format($home->advance_rent) }}
               บาท</span>
       </p>
-      <div class="space-between">
-          <p class="text-content-black margin-bottom-8">
-              <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-              เงินดาวน์ <span class="ml-16"> {{ number_format($home->sell_price) }}
-                  บาท</span>
-          </p>
-          <span>-</span>
-          <p class="text-content-black margin-bottom-8">
-              <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-              ผ่อนดาวน์ {{ $home->down_payment_installments }}
-
-          </p>
-      </div>
-      @if ($home->down_payment_installments == 'ได้')
-          <div class="space-between">
-              <p class="text-content-black margin-bottom-8">
-                  <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-                  ผ่อนได้ <span class="ml-16">{{ $home->installments }} งวด</span>
+      <div class="flex-direction-break-word">
+          <div class="w-50">
+              <p class="text-content-black margin-bottom-8 space-between">
+                  <span>
+                      <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                      เงินดาวน์ <span class="ml-8"> {{ number_format($home->sell_price) }}
+                          บาท</span>
+                  </span>
+                  <span>-</span>
               </p>
-              <span>-</span>
-              <p class="text-content-black margin-bottom-8">
-                  <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
-                  งวดละ <span class="ml-16">{{ number_format($home->each_installment) }} บาท
-              </p>
+              @if ($home->down_payment_installments == 'ได้')
+                  <p class="text-content-black margin-bottom-8 space-between">
+                      <span><img class="icon-content-2"
+                              src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                          ผ่อนได้ <span class="ml-16">{{ $home->installments }} งวด</span></span>
+                      <span>-</span>
+                  </p>
+              @endif
           </div>
-      @endif
+          <div class="w-50">
+              <p class="text-content-black margin-bottom-8">
+                  <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                  ผ่อนดาวน์ {{ $home->down_payment_installments }}
+
+              </p>
+              @if ($home->down_payment_installments == 'ได้')
+                  <p class="text-content-black margin-bottom-8">
+                      <img class="icon-content-2" src="{{ URL::asset('/assets/image/home/pajamas_sort-lowest.png') }}">
+                      งวดละ <span class="ml-16">{{ number_format($home->each_installment) }} บาท
+                  </p>
+              @endif
+          </div>
+      </div>
+
 
   </div>
