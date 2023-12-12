f7645ef76f38b1ddfd168405999237c2815c04f7
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-12-13 00:54:17 +0700

    เพิ่ม

--- resources/views/admin/addInputJs.blade.php
+++ resources/views/admin/addInputJs.blade.php
@@ -3,15 +3,22 @@
         var counter = 0;
 
         $("#add-input").click(function() {
-            // Increment the counter for a unique name
-            counter++;
+            // Display a confirmation dialog
+            var userConfirmation = confirm("เพิ่มเติมช่องสิ่งอำนวยความสะดวก");
 
-            // Create a new input element with a unique name
-            var newInput = $("<input type='text' class='form-control mt-2' name='thereVarious[" +
-                counter + "]' placeholder='เพิ่มเติม สิ่งอำนวยความสะดวก'>");
+            // Check if the user clicked "OK"
+            if (userConfirmation) {
+                // Increment the counter for a unique name
+                counter++;
 
-            // Append the new input to the container
-            $("#input-container").append(newInput);
+                // Create a new input element with a unique name
+                var newInput = $("<input type='text' class='form-control mt-2' name='thereVarious[" +
+                    counter + "]' placeholder='เพิ่มเติม สิ่งอำนวยความสะดวก'>");
+
+                // Append the new input to the container
+                $("#input-container").append(newInput);
+            }
+            // If the user clicked "Cancel," do nothing
         });
     });
 </script>
